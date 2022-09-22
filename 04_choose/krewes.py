70d1711595ccdcb03b18c797f2bce0f5eee9f986
Daniel He <dhe30@stuy.edu>
2022-09-22 13:53:56 -0400

    added krewes

---
+++ 04_choose/krewes.py
@@ -0,0 +1,9 @@
+# <Frist> <Lsat>
+# SoftDev
+# K<nn> -- <Title/Topic/Summary... (Aim for concision, brevity, CLARITY. Write to your future self...)>
+# 2022-09-22
+# time spent: <elapsed time in hours, rounded to nearest tenth>
+
+# DISCO: 2, 7, 8 is the period numbers
+# QCC:
+# OPS SUMMARY: 
