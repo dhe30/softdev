0abaf15d91097a5191d6ae23154d51525a41ce37
Daniel He <dhe30@stuy.edu>
2022-10-26 13:28:23 -0400

    finished task, need to document things

--- 18_csv2db/db_builder.py
+++ 18_csv2db/db_builder.py
@@ -1,7 +1,8 @@
-#Clyde "Thluffy" Sinclair
+#Team Green Monkeys; Daniel He, Faiyaz Rafee
 #SoftDev  
-#skeleton/stub :: SQLITE3 BASICS
-#Oct 2022
+#K18 SQLITE3 BASICS- making tables and adding data from csv files in sqlite through python
+#2022-10-26
+#time spent: 1.5 hrs
 
 import sqlite3   #enable control of an sqlite database
 import csv       #facilitate CSV I/O
@@ -11,18 +12,28 @@ DB_FILE="discobandit.db"
 
 db = sqlite3.connect(DB_FILE) #open if file exists, otherwise create
 c = db.cursor()               #facilitate db ops -- you will use cursor to trigger db events
-
+c.execute("DROP TABLE if exists students")
+c.execute("DROP TABLE if exists courses")
 #==========================================================
 
 
 # < < < INSERT YOUR TEAM'S POPULATE-THE-DB CODE HERE > > >
-courses = csv.DictReader(open("courses.csv", "r"))
-c.execute("create table courses (code TEXT, mark INTEGER, id INTEGER);")
-for course in courses: # creates an object where each row is a dictonary
-    command = "insert into courses values (\"" + course['code'] + "\", " + course['mark'] + ", " + course['id'] + ");"          # test SQL stmt in sqlite3 shell, save as string
-    print(command)
-    c.execute(command)    # run SQL statement
+def reading_it(a):
+    with open(a, newline = "") as file:
+        courses = csv.DictReader(file)
+        table_name = a[:-4]
+        print(table_name)
+        c.execute("create table " + table_name + " (code TEXT, mark INTEGER, id INTEGER);")
+        for course in courses: # creates an object where each row is a dictonary
+            if a == "courses.csv":
+                command = "insert into " + table_name + " values (\"" + course['code'] + "\", " + course['mark'] + ", " + course['id'] + ");"          # test SQL stmt in sqlite3 shell, save as string
+            else: 
+                command = "insert into " + table_name + " values (\"" + course['name'] + "\", " + course['age'] + ", " + course['id'] + ");"          # test SQL stmt in sqlite3 shell, save as string
 
+            print(command)
+            c.execute(command)    # run SQL statement
+reading_it("courses.csv")
+reading_it("students.csv")
 #==========================================================
 
 db.commit() #save changes
--- 18_csv2db/readme.md
+++ 18_csv2db/readme.md
@@ -1,3 +1,5 @@
 ### Discoveri
 
 * YOU cannot concatenate a string with an integer with the + operator in python 
+* with open() opens and subsequently closes the file without needing file.close() to be stated
+* " is "\"" in string form
